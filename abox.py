64802d3b141829b7058f2ae2830ca74a84e78623
gabozarate14 <gabozarate14@gmail.com>
2023-05-14 20:40:28 +0200

    first commit

---
+++ abox.py
@@ -0,0 +1,218 @@
+import pandas as pd
+import re
+from rdflib import Graph, Namespace, Literal, URIRef
+from unidecode import unidecode
+from rdflib.namespace import RDF, RDFS, XSD
+
+PAPERS_SOURCE = 'data/papers.csv'
+AUTHORS_SOURCE = 'data/authors.csv'
+WRITES_SOURCE = 'data/writes.csv'
+CONFERENCES_SOURCE = 'data/conference.csv'
+JOURNAL_SOURCE = 'data/journal.csv'
+VOLUME_SOURCE = 'data/volume.csv'
+PROCEEDING_SOURCE = 'data/proceeding.csv'
+EDITION_SOURCE = 'data/edition.csv'
+PUBLICATION_SOURCE = 'data/publication.csv'
+PUBLISHES_SOURCE = 'data/publishes.csv'
+PRESENTED_SOURCE = 'data/presented.csv'
+REVIEWS_SOURCE = 'data/review_evolved.csv'
+BELONGS_TO_CONFERENCE = 'data/belongs_to_conference.csv'
+BELONGS_TO_JOURNAL = 'data/belongs_to_journal.csv'
+CHAIR_DESIGNATES_SOURCE = 'data/chair_designates.csv'
+EDITOR_DESIGNATES_SOURCE = 'data/editor_designates.csv'
+AREA_SOURCE = 'data/area.csv'
+RELATED_TO_SOURCE = 'data/related_to.csv'
+PAPER_ACCEPTED_SOURCE = 'data/paper_accepted.csv'
+
+
+def clean_uri(text):
+    reserved_chars_pattern = r'[:/¿?#\[\]@!$&\'()*+,;=."]'
+    clean_text = re.sub(r"\s+", "_", unidecode(text).strip())
+    return re.sub(reserved_chars_pattern, "", clean_text)
+
+
+g = Graph()
+mainURI = "http://www.semanticweb.org/eloic/ontologies/2023/4/untitled-ontology-4/"
+ns = Namespace(mainURI)
+
+# ABOX
+
+# Load :author
+authors = pd.read_csv(AUTHORS_SOURCE)
+for _, row in authors.iterrows():
+    node = clean_uri(row["author"])
+    g.add((ns[node], RDF.type, ns.author))
+    g.add((ns[node], ns.name, Literal(row["author"])))
+
+# Load :paper
+papers = pd.read_csv(PAPERS_SOURCE)
+for _, row in papers.iterrows():
+    node = clean_uri(row["title"])
+    node_type = clean_uri(row["type"])
+    g.add((ns[node], RDF.type, ns[node_type]))
+    g.add((ns[node], ns.title, Literal(row["title"])))
+
+# Load :authorship
+authorships = pd.read_csv(WRITES_SOURCE)
+for _, row in authorships.iterrows():
+    paper_node = URIRef(mainURI+clean_uri(row["title"]))
+    author_node = URIRef(mainURI+clean_uri(row["author"]))
+
+    # create the authorship triple
+    g.add((author_node, ns.authorship, paper_node))
+
+# Load :conference
+conferences = pd.read_csv(CONFERENCES_SOURCE)
+for _, row in conferences.iterrows():
+    node = clean_uri(row["name"])
+    node_type = clean_uri(row["type"])
+    g.add((ns[node], RDF.type, ns[node_type]))
+    g.add((ns[node], ns.name, Literal(row["name"])))
+
+# Load :proceeding
+proceedings = pd.read_csv(PROCEEDING_SOURCE)
+for _, row in proceedings.iterrows():
+    node = f"proceeding_{row['proceeding']}"
+    g.add((ns[node], RDF.type, ns.proceeding))
+    g.add((ns[node], ns.name, Literal(row["proceeding"])))
+    g.add((ns[node], ns.year, Literal(row["year"])))
+
+# Load :has_edition
+editions = pd.read_csv(EDITION_SOURCE)
+for _, row in editions.iterrows():
+    conference = URIRef(mainURI+clean_uri(row["conference"]))
+    proceeding = URIRef(f'{mainURI}proceeding_{row["proceeding"]}')
+
+    # create the authorship triple
+    g.add((conference, ns.has_edition, proceeding))
+
+# Load :journal
+journals = pd.read_csv(JOURNAL_SOURCE)
+for _, row in journals.iterrows():
+    node = clean_uri(row["name"])
+    g.add((ns[node], RDF.type, ns.journal))
+    g.add((ns[node], ns.name, Literal(row["name"])))
+
+# Load :volume
+volumes = pd.read_csv(VOLUME_SOURCE)
+for _, row in volumes.iterrows():
+    node = f"volume_{row['volume']}"
+    g.add((ns[node], RDF.type, ns.volume))
+    g.add((ns[node], ns.name, Literal(row["volume"])))
+    g.add((ns[node], ns.year, Literal(row["year"])))
+
+# Load :publish
+publishes = pd.read_csv(PUBLICATION_SOURCE)
+for _, row in publishes.iterrows():
+    journal = URIRef(mainURI+clean_uri(row["journal"]))
+    volume = URIRef(f'{mainURI}volume_{row["volume"]}')
+
+    # create the authorship triple
+    g.add((journal, ns.publish, volume))
+
+# Load :submit_to_volume
+submit_to_volume = pd.read_csv(PUBLISHES_SOURCE)
+for _, row in submit_to_volume.iterrows():
+    paper_node = URIRef(mainURI + clean_uri(row["article"]))
+    volume_node = URIRef(f'{mainURI}volume_{row["volume"]}')
+    g.add((paper_node, ns.submit_to_volume, volume_node))
+
+# Load :submit_to_proceeding
+submit_to_proceeding = pd.read_csv(PRESENTED_SOURCE)
+for _, row in submit_to_proceeding.iterrows():
+    paper_node = URIRef(mainURI + clean_uri(row["article"]))
+    proceeding_node = URIRef(f'{mainURI}proceeding_{row["proceeding"]}')
+    g.add((paper_node, ns.submit_to_proceeding, proceeding_node))
+
+
+# Load review
+reviews = pd.read_csv(REVIEWS_SOURCE)
+for index, row in reviews.iterrows():
+    # :reviewer
+    reviewer = clean_uri(row["reviewer"])
+    g.add((ns[reviewer], RDF.type, ns.reviewer))
+    g.add((ns[reviewer], ns.name, Literal(row["reviewer"])))
+    # :review
+    review = f"review_{index}"
+    g.add((ns[review], RDF.type, ns.review))
+    g.add((ns[review], ns.decision_result, Literal(row["decision"])))
+    g.add((ns[review], ns.decision_text, Literal(row["content"])))
+    # :writes_review
+    reviewer_node = URIRef(mainURI + reviewer)
+    review_node = URIRef(mainURI + review)
+    g.add((reviewer_node, ns.writes_review, review_node))
+    # :review_of_paper
+    paper_node = URIRef(mainURI+clean_uri(row["article"]))
+    g.add((review_node, ns.review_of_paper, paper_node))
+
+# Load :accepted (for papers to be acknowledged as publications)
+accepted = pd.read_csv(PAPER_ACCEPTED_SOURCE)
+for _, row in accepted.iterrows():
+    node = clean_uri(row["article"])
+    decision = "accepted" if row["final_decision"] else "not accepted"
+    g.add((ns[node], ns.accepted, Literal(decision)))
+
+# Load chair
+chairs = pd.read_csv(BELONGS_TO_CONFERENCE)
+for _, row in chairs.iterrows():
+    # :chair
+    node = clean_uri(row["chair"])
+    g.add((ns[node], RDF.type, ns.chair))
+    g.add((ns[node], ns.name, Literal(row["chair"])))
+    # :belongs_to_conference
+    chair_node = URIRef(mainURI + node)
+    conference_node = URIRef(mainURI + clean_uri(row["conference"]))
+    g.add((chair_node, ns.belongs_to_conference, conference_node))
+
+# Load :chair_designates
+chair_designates = pd.read_csv(CHAIR_DESIGNATES_SOURCE)
+for _, row in chair_designates.iterrows():
+    chair_node = URIRef(mainURI + clean_uri(row["chair"]))
+    reviewer_node = URIRef(mainURI + clean_uri(row["reviewer"]))
+    g.add((chair_node, ns.chair_designates, reviewer_node))
+
+# Load editor
+editors = pd.read_csv(BELONGS_TO_JOURNAL)
+for _, row in editors.iterrows():
+    # :editor
+    node = clean_uri(row["editor"])
+    g.add((ns[node], RDF.type, ns.editor))
+    g.add((ns[node], ns.name, Literal(row["editor"])))
+    # :belongs_to_journal
+    editor_node = URIRef(mainURI + node)
+    journal_node = URIRef(mainURI + clean_uri(row["journal"]))
+    g.add((editor_node, ns.belongs_to_journal, journal_node))
+
+# Load :editor_designates
+editor_designates = pd.read_csv(EDITOR_DESIGNATES_SOURCE)
+for _, row in editor_designates.iterrows():
+    editor_node = URIRef(mainURI + clean_uri(row["editor"]))
+    reviewer_node = URIRef(mainURI + clean_uri(row["reviewer"]))
+    g.add((editor_node, ns.editor_designates, reviewer_node))
+
+# Load :area
+areas = pd.read_csv(AREA_SOURCE)
+for _, row in areas.iterrows():
+    node = clean_uri(row["area"])
+    g.add((ns[node], RDF.type, ns.area))
+    g.add((ns[node], ns.name, Literal(row["area"])))
+
+# Load item related to area
+related_to = pd.read_csv(RELATED_TO_SOURCE)
+for _, row in related_to.iterrows():
+    # Si se cambia a un related por tipo agregar if
+    main_node = URIRef(mainURI + clean_uri(row["name"]))
+    area_node = URIRef(mainURI + clean_uri(row["area"]))
+    if row["type"] == 'paper':
+        g.add((main_node, ns.paper_area, area_node))
+    elif row["type"] == 'conference':
+        g.add((main_node, ns.conference_area, area_node))
+    elif row["type"] == 'journal':
+        g.add((main_node, ns.journal_area, area_node))
+
+
+# # Print the graph
+# print(g.serialize(format="turtle"))
+
+# Save the graph
+g.serialize(format="turtle", destination="graph/abox.ttl")
---
+++ generateData.py
@@ -0,0 +1,154 @@
+import pandas as pd
+import random
+
+REVIEWS_SOURCE = 'data/review_evolved.csv'
+NAMES_SOURCE = 'data/names_gen.csv'
+PUBLISHES_SOURCE = 'data/publishes.csv'
+PRESENTED_SOURCE = 'data/presented.csv'
+PROCEEDING_SOURCE = 'data/proceeding.csv'
+EDITION_SOURCE = 'data/edition.csv'
+PUBLICATION_SOURCE = 'data/publication.csv'
+CONFERENCES_SOURCE = 'data/conference.csv'
+JOURNAL_SOURCE = 'data/journal.csv'
+PAPER_SOURCE = 'data/papers.csv'
+
+
+# Output routes
+OUTPUT_BELONGS_TO_CONFERENCE = 'data/belongs_to_conference.csv'
+OUTPUT_BELONGS_TO_JOURNAL = 'data/belongs_to_journal.csv'
+OUTPUT_CHAIR_DESIGNATES = 'data/chair_designates.csv'
+OUTPUT_EDITOR_DESIGNATES = 'data/editor_designates.csv'
+OUTPUT_RELATED_TO = 'data/related_to.csv'
+OUTPUT_PAPER_ACCEPTED = 'data/paper_accepted.csv'
+OUTPUT_AREA = 'data/area.csv'
+
+reviews = pd.read_csv(REVIEWS_SOURCE)
+names = pd.read_csv(NAMES_SOURCE)
+publishes = pd.read_csv(PUBLISHES_SOURCE)
+presented = pd.read_csv(PRESENTED_SOURCE)
+edition = pd.read_csv(EDITION_SOURCE)
+publication = pd.read_csv(PUBLICATION_SOURCE)
+conferences = pd.read_csv(CONFERENCES_SOURCE)
+journals = pd.read_csv(JOURNAL_SOURCE)
+papers = pd.read_csv(PAPER_SOURCE)
+areas = pd.read_csv(OUTPUT_AREA)
+
+random.seed(123)
+
+name_list = list(names['names'].unique())
+n = len(name_list)
+chairs = name_list[:n//2]
+editors = name_list[n//2:]
+
+# # Generate chairs relations
+
+# :belongs_to_conference
+conf_list = list(conferences['name'].unique())
+chair_df = pd.DataFrame(columns=['chair', 'conference'])
+for chair in chairs:
+    conf = random.choice(conf_list)
+    row_data = {'chair': chair, 'conference': conf}
+    chair_df = pd.concat([chair_df, pd.DataFrame([row_data])], ignore_index=True)
+
+chair_df.to_csv(OUTPUT_BELONGS_TO_CONFERENCE,encoding='utf-8',index=False)
+
+# :chair_designates
+conf_merge = pd.merge(conferences, chair_df, left_on='name', right_on='conference')
+conf_merge = pd.merge(conf_merge, edition, left_on='conference', right_on='conference')
+conf_merge = pd.merge(conf_merge, presented, left_on='proceeding', right_on='proceeding')
+conf_merge = pd.merge(conf_merge, reviews, left_on='article', right_on='article')
+result_df = conf_merge.loc[:, ['chair', 'reviewer']].drop_duplicates()
+
+unique_reviewers = result_df['reviewer'].unique()
+random_chairs = [random.choice(result_df[result_df['reviewer'] == reviewer]['chair'].tolist()) for reviewer in unique_reviewers]
+new_data = {'chair': random_chairs, 'reviewer': unique_reviewers}
+chair_designates = pd.DataFrame(new_data)
+
+chair_designates.to_csv(OUTPUT_CHAIR_DESIGNATES,encoding='utf-8',index=False)
+
+# Generate editors relations
+
+# :belongs_to_journal
+jour_list = list(journals['name'].unique())
+editor_df = pd.DataFrame(columns=['editor', 'journal'])
+for editor in editors:
+    jour = random.choice(jour_list)
+    row_data = {'editor': editor, 'journal': jour}
+    editor_df = pd.concat([editor_df, pd.DataFrame([row_data])], ignore_index=True)
+
+editor_df.to_csv(OUTPUT_BELONGS_TO_JOURNAL,encoding='utf-8',index=False)
+
+# :editor_designates
+jour_merge = pd.merge(journals, editor_df, left_on='name', right_on='journal')
+jour_merge = pd.merge(jour_merge, publication, left_on='journal', right_on='journal')
+jour_merge = pd.merge(jour_merge, publishes, left_on='volume', right_on='volume')
+jour_merge = pd.merge(jour_merge, reviews, left_on='article', right_on='article')
+result_df = jour_merge.loc[:, ['editor', 'reviewer']].drop_duplicates()
+
+unique_reviewers = result_df['reviewer'].unique()
+random_editor = [random.choice(result_df[result_df['reviewer'] == reviewer]['editor'].tolist()) for reviewer in unique_reviewers]
+new_data = {'editor': random_editor, 'reviewer': unique_reviewers}
+editor_designates = pd.DataFrame(new_data)
+
+editor_designates.to_csv(OUTPUT_EDITOR_DESIGNATES,encoding='utf-8',index=False)
+
+# Generate links with area and paper, conference and journal
+area_list = list(set(papers['key_words'].str.split('|', expand=True).stack()))
+area_df = pd.DataFrame({'area': area_list})
+
+area_df.to_csv(OUTPUT_AREA, index=False)
+
+related_to_df = papers.assign(area=papers['key_words'].str.split('|')).explode('area')
+related_to_df = related_to_df.reset_index(drop=True)
+related_to_df = related_to_df.rename(columns={'title': 'name'})
+related_to_df = related_to_df.loc[:, ['name', 'area']]
+related_to_df = related_to_df.assign(type='paper')
+
+for _,row in conferences.iterrows():
+    for _ in range(random.randint(1,5)):
+        area = random.choice(area_list)
+        row_data = {'type': 'conference', 'name': row['name'], 'area': area}
+        related_to_df = pd.concat([related_to_df, pd.DataFrame([row_data])], ignore_index=True)
+
+for _,row in journals.iterrows():
+    for _ in range(random.randint(1,7)):
+        area = random.choice(area_list)
+        row_data = {'type': 'journals', 'name': row['name'], 'area': area}
+        related_to_df = pd.concat([related_to_df, pd.DataFrame([row_data])], ignore_index=True)
+
+related_to_df = related_to_df.drop_duplicates()
+related_to_df.to_csv(OUTPUT_RELATED_TO,index=False)
+
+# Generate paper accepted
+
+final_decision = reviews.groupby('article')['decision'].apply(lambda x: (x == 'accepted').sum() / len(x) >= 0.5)
+final_decision_df = pd.DataFrame({'article': final_decision.index, 'final_decision': final_decision.values})
+
+final_decision_df.to_csv(OUTPUT_PAPER_ACCEPTED,encoding='utf-8',index=False)
+
+# Generate paper subtypes
+paper_subtypes = ['full paper', 'short paper', 'demo_paper']
+
+# poster will be handled only for conferences
+papers_conf = presented['article'].unique()
+
+col_type = []
+for _, row in papers.iterrows():
+    # if it was sent to a conference it can be poster
+    type = None
+    if row['title'] in papers_conf:
+        if random.choice([True, False]):
+            type = 'poster'
+    if not type:
+        type = random.choice(paper_subtypes)
+    col_type.append(type)
+
+papers["type"] = col_type
+papers.to_csv(PAPER_SOURCE,index=False)
+
+
+
+
+
+
+
